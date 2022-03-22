9eae53d813fce4830e9775823a5e5691f91ae0ab
AltairDaMasyaf <95611920+AltairDaMasyaf@users.noreply.github.com>
2022-03-22 20:18:40 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+// Change Nav Color //
+
+$(document).ready(function () {
+  var scroll_start = 0;
+  var startchange = $(".hero");
+  var offset = startchange.offset();
+  if (startchange.length) {
+    $(document).scroll(function () {
+      scroll_start = $(this).scrollTop();
+      if (scroll_start > offset.top) {
+        // $(".nav").css("background-color", "#000");
+        $(".nav").css("border-bottom", "2px solid #f9b427");
+      } else {
+        // $(".nav").css("background-color", "transparent");
+        $(".nav").css("border-bottom", "2px solid white");
+      }
+    });
+  }
+});
